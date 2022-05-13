3632fbe9ee8027d297f890bde41c1dd4d1d95754
Sebastian Toledo <seba95matias@hotmail.com>
2022-05-13 01:05:00 -0300

    last exercise

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FocusableInput } from "./components/FocusableInput";
 import { ImageGallery } from "./components/ImageGallery";
 import { PlayerStatus } from "./components/PlayerStatus";
 import { TeamsList } from "./components/TeamsList";
-
+import { ListItemsForNavigation } from "./components/ListItemsForNavigation";
 import "./App.css";
 import { Grocery } from "./components/Grocery";
 import { Rating } from "./components/Rating";
@@ -17,6 +17,12 @@ const LINKS = [
   "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
   "https://media.istockphoto.com/photos/argentina-buenos-aires-skyline-puerto-madero-at-night-picture-id940492176?k=20&m=940492176&s=612x612&w=0&h=CqlKHoGvHkNyPmoRd4Qy_oON9F6xxLWgnhYvzKR6OjQ=",
 ];
+
+
+const ITEM_LIST = Array(10).fill({
+  name: "juan",
+  status: false
+});
 
 export default function App() {
   return (
@@ -42,6 +48,9 @@ export default function App() {
       <br />
       <h3>'Rating' test</h3>
       <Rating />
+      <br />
+      <h3>'ListItemsForNavigation' test</h3>
+      <ListItemsForNavigation itemList={ITEM_LIST} />
     </div>
   );
 }
--- src/components/ListItemsForNavigation.jsx
+++ src/components/ListItemsForNavigation.jsx
@@ -12,37 +12,47 @@
 
 import { useEffect, useRef, useState } from "react";
 
-// Simulating a list of items to render.
-// This can be passed through props as well. The constant is declared here for convenience
-const itemsList = Array(10).fill({
-	/** Add the properties you consider, there are no specific requirements related to what you have to render. Be creative :) */
-});
-
 export function ListItemsForNavigation(props) {
-	const [
-		selectedIndex,
-		setSelectedIndex,
-	] = useState(/** Initialize the state as you need */);
-	const activeItemRef = useRef();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const activeItemRef = useRef();
 
-	useEffect(
-		function () {
-			// Focus the item using this effect
-		},
-		[
-			/* Use accordingly the dependencies */
-		]
-	);
+  useEffect(
+    function () {
+      activeItemRef.current.focus();
+    },
+    [selectedIndex]
+  );
 
-	function handleKeyDown(event) {
-		// Add the proper logic to calculate the index that correspond to the item that should be focused.
-	}
+  function handleKeyDown(event) {
+    // Add the proper logic to calculate the index that correspond to the item that should be focused.
+    if (
+      selectedIndex < props.itemList.length - 1 &&
+      (event.key === "ArrowDown" || event.key === "ArrowLeft")
+    ) {
+      setSelectedIndex((prevIndex) => prevIndex + 1);
+    }
+    if (
+      selectedIndex > 0 &&
+      (event.key === "ArrowUp" || event.key === "ArrowRight")
+    ) {
+      setSelectedIndex((prevIndex) => prevIndex - 1);
+    }
+  }
 
-	return (
-		<ul onKeyDown={handleKeyDown}>
-			{/** Render itemsList as you wish, probably you want to render <li></li> with the proper attributes */}
-			{/** If you have issues focusing an element, it is probably because the element is not focusable originally. Try with tabIndex={0} */}
-			{/** Do not forget to pass the reference to the selected item */}
-		</ul>
-	);
+  return (
+    <ul onKeyDown={handleKeyDown} tabIndex={0}>
+      {props.itemList.map((item, index) => (
+        <li key={index} ref={activeItemRef}>
+          <input
+            ref={index === selectedIndex ? activeItemRef : null}
+            value={item.name}
+          />
+        </li>
+      ))}
+      {/** Render itemsList as you wish, probably you want to render <li></li> with the proper attributes */}
+      {/** If you have issues focusing an element, it is probably because the element is not focusable originally.
+       * Try with tabIndex={0} */}
+      {/** Do not forget to pass the reference to the selected item */}
+    </ul>
+  );
 }
